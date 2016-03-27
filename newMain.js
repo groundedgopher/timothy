de5c61a53ac2d5b53010c5c3ebb732542f4c5687
U-EPS\tsmith <tsmith@eps.cm>
2016-03-26 23:47:33 -0500

    node junk

--- newMain.js
+++ newMain.js
@@ -3,9 +3,13 @@
  */
 
 
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World Nodejs run on Amazon EC2 by me\n');
-}).listen(80);
-console.log('Server running on Amazon EC2');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
