e554539e176f93ab9a37974d59d96a9190d77169
U-EPS\tsmith <tsmith@eps.cm>
2016-03-26 22:57:01 -0500

    learning node

--- newMain.js
+++ newMain.js
@@ -2,4 +2,13 @@
  * Created by tsmith on 3/26/2016.
  */
 
-console.log("Hello, World!");
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at something');
