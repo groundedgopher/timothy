ce47f3cb7c8dadff7122353dcb5e07b4d9be5698
U-EPS\tsmith <tsmith@eps.cm>
2016-03-27 20:11:39 -0500

    node

--- newMain.js
+++ newMain.js
@@ -9,6 +9,6 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     response.end('Hello World\n');
-}).listen(80);
+}).listen(8000);
 
 console.log('Server running at something');
