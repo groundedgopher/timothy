41efdce0bf1ff305974560073ab5a5c18545787d
Tim <timsemail@cox.net>
2016-04-20 17:05:20 -0500

    blah

--- node/main.js
+++ node/main.js
@@ -8,7 +8,7 @@ var http = require('http').Server(app);
 
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello World!!</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(8081, function(){
